cccaae696abe1696e8e2cea361b5c504b865ed22
Veronika Yeremenko <ver.sab1eremenk0@gmail.com>
2025-02-11 19:49:40 +0200

    task 1
    calculates the number of days between a given date and the current date

--- hw_3_1.py
+++ hw_3_1.py
@@ -4,5 +4,5 @@ def get_days_from_today(date):
     date = datetime.strptime(date, '%Y-%m-%d').date()
     return (date - datetime.today().date()).days
 
-date = input("Enter date (yyyy-mm-dd):")
+date = input("Enter your date (yyyy-mm-dd):")
 print(get_days_from_today(date))  
