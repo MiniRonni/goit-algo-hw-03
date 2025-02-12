de1c00d35d6c1ed8dc30859d98f489dbb051d795
Veronika Yeremenko <ver.sab1eremenk0@gmail.com>
2025-02-12 14:13:13 +0200

    Update hw_3_2.py

--- hw_3_2.py
+++ hw_3_2.py
@@ -1,13 +1,26 @@
 import random
 
 def get_number_ticket(min, max, quantity):
-    result_arr = set()
-
-    while len(result_arr) != quantity:
-        result_arr.add(random.randint(min, max))
-
-    numbers = sorted(list(result_arr))
+    """
+    Generate a set of unique random numbers within the specified range.
     
-    return numbers
+    Args:
+        min (int): The minimum value of the range (>=1).
+        max (int): The maximum value of the range (<= 1000).
+        quantity (int): The number of unique random numbers to generate.
 
-print(f"Лотерейні числа: {get_number_ticket(1, 55, 6)}")
+    Returns:
+        list: A list of unique random numbers.
+              Return an empty list if parameters are invalid.
+    """
+    if not (1 <= min <= 1000 and 1 <= max <= 1000 and min <= max):
+        return []
+    
+    if not (min <= quantity <= max):
+        return []
+
+    numbers = random.sample(range(min, max + 1), quantity)
+    
+    return sorted(numbers)
+
+print(f"Лотерейні числа: {get_number_ticket(1, 46, 6)}")
