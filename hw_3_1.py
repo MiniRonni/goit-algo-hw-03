dc584ef19989d974064681e4dcdd5677e5655227
Veronika Yeremenko <ver.sab1eremenk0@gmail.com>
2025-02-12 14:12:01 +0200

    task 1

--- hw_3_1.py
+++ hw_3_1.py
@@ -1,8 +1,29 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    date = datetime.strptime(date, '%Y-%m-%d').date()
-    return (date - datetime.today().date()).days
+    """
+    Calculates the number of days between a given date and the current date.
+    
+    Args:
+        date_str (str): A date string in the format 'YYYY-MM-DD'.
+        
+    Returns:
+        int: The number of days between the given day and today.
+        
+    Raises:
+        ValueError: If the date format is incorrect.
+    """
+    try:
+        date = datetime.strptime(date, '%Y-%m-%d').date()
+        today = datetime.today().date()
+        return (date - today).days
+    except ValueError:
+        raise ValueError("Incorrect date format, should be YYYY-MM-DD")
 
-date = input("Enter your date (yyyy-mm-dd):")
-print(get_days_from_today(date))  
+
+if __name__:
+    date = input("Enter your date (yyyy-mm-dd): ").strip()
+    try:
+        print(get_days_from_today(date))  
+    except ValueError as e:
+        print(e)
